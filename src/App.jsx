2494a4218a161a1034f8042a818f4052ca6d858d
Camila Alm <camila.salmeidatech@hotmail.com>
2024-11-07 00:18:15 -0500

    setting up state for App function

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
-import NewTodo from './components/NewTodo.jsx'
+import NewTodo from "./components/NewTodo.jsx";
+import { useState } from 'react';
+
 
 const App = () => {
-  
+  const [todos, setTodos ] = useState([]);
+
   const addTodo = (newTodo) => {
-    console.log('This is adding the New Todo: ', newTodo);
+    const updatedTodos = [...todos, newTodo];                            // Start with a brand new empty array inside addTodo.
+    console.log("This is adding the New Todo: ", newTodo);
+    console.log(updatedTodos);
+    setTodos(updatedTodos);                                              // Pass this new array into `setTodos`.
+
   };
 
   return (
     <>
-    <h1>To-do App </h1>
-    <NewTodo addTodo={addTodo}/>
+      <h1>To-do App </h1>
+      <NewTodo addTodo={addTodo} />
     </>
-  )
-}
+  );
+};
+
 
 export default App;
