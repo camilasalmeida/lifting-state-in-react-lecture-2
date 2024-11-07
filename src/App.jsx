e967ef2a9be008aca3a472782e28463e4e2e6c1b
Camila Alm <camila.salmeidatech@hotmail.com>
2024-11-07 00:25:34 -0500

    display the to-dos

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
 import NewTodo from "./components/NewTodo.jsx";
-import { useState } from 'react';
-
+import { useState } from "react";
 
 const App = () => {
-  const [todos, setTodos ] = useState([]);
+  const [todos, setTodos] = useState([]);
 
   const addTodo = (newTodo) => {
-    const updatedTodos = [...todos, newTodo];                            // Start with a brand new empty array inside addTodo.
+    const updatedTodos = [...todos, newTodo];                              // Start with a brand new empty array inside addTodo.
     console.log("This is adding the New Todo: ", newTodo);
     console.log(updatedTodos);
-    setTodos(updatedTodos);                                              // Pass this new array into `setTodos`.
-
+    setTodos(updatedTodos);                                                // Pass this new array into `setTodos`.
   };
 
   return (
     <>
       <h1>To-do App </h1>
       <NewTodo addTodo={addTodo} />
+      <h3>List of your To-do:</h3>
+      <ul>
+        {todos.map((todo, index) => {
+         return <li key={index}>{todo}</li>;                               // Don't forget the return.
+        })}
+      </ul>
     </>
   );
 };
-
 
 export default App;
